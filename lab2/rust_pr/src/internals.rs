8bcf7e0b21f9dd26b59b98570bc69903ac909c28
Kripaka Illia <kripakaillya@gmail.com>
2023-11-15 23:04:53 +0200

    create base functions for calculation * make_n_gram * make_frequency_table * make_probability_table * calculate_entropy

--- lab2/rust_pr/src/internals.rs
+++ lab2/rust_pr/src/internals.rs
@@ -1,30 +1,84 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+
 use regex::Regex;
 
-pub(super) fn make_frequency_table(filepath: &str, alphabet: Vec<String>) -> HashMap<String, u64> {
-    let map = HashMap::new();
-    let file = File::open(filepath).unwrap();
+pub fn make_frequency_table(
+    filepath: &str,
+    alphabet: &Vec<String>,
+    chunks: usize,
+) -> HashMap<String, u64> {
+    let mut file = File::open(filepath).unwrap();
+    let mut content = String::new();
+    file.read_to_string(&mut content);
 
+    //trim string to specific size
+    if content.len() % chunks != 0 {
+        for _ in (0..(content.len() % chunks)) {
+            content.remove(content.len() - 1);
+        }
+    }
 
+    let mut chars = content.chars();
+    let mut sub_string = (0..)
+        .map(|_| chars.by_ref().take(chunks).collect::<String>())
+        .take_while(|s| !s.is_empty())
+        .collect::<Vec<_>>();
+    sub_string.remove(sub_string.len() - 1);
 
+    let mut map = alphabet
+        .iter()
+        .map(|x| (x.clone(), 0))
+        .collect::<HashMap<String, u64>>();
 
+    println!("substr {:?}", sub_string.len());
+
+    for x in sub_string {
+        match map.get(&x) {
+            None => {
+                println!("{x} isn't in map, \n{:?}", 5)
+            }
+            Some(_) => {}
+        }
+        map.insert(x.clone(), map.get(&x).unwrap() + 1);
+    }
     map
 }
 
-pub(super) fn make_n_gram() -> HashMap<String, u64> {
-
-    todo!()
+pub fn make_n_gram(n: usize, alphabet: &[char]) -> Vec<String> {
+    let mut vec = alphabet
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    for _ in 1..n {
+        let mut tmp = Vec::new();
+        for x in alphabet {
+            let mut bunches_of_n_gram = vec.clone();
+            bunches_of_n_gram = bunches_of_n_gram
+                .iter()
+                .map(|y| (y.clone()) + x.to_string().as_str())
+                .collect();
+            tmp.extend_from_slice(&bunches_of_n_gram);
+        }
+        vec = tmp
+    }
+    vec
 }
 
-pub(super) fn calculate_entropy() -> HashMap<String, u64> {
-    todo!()
-}
-pub(super) fn make_probability_table() -> HashMap<String, u64> {
-    todo!()
+pub fn make_probability_table(freq_table: &HashMap<String, u64>) -> HashMap<String, f64> {
+    let sum = freq_table.iter().fold(0, |acc, (_, val)| acc + val);
+    freq_table
+        .iter()
+        .map(|(key, val)| (key.clone(), (*val as f64 / (sum as f64))))
+        .collect()
 }
 
+pub fn calculate_entropy(prob_table: &HashMap<String, f64>) -> f64 {
+   prob_table.iter().fold(0., |acc, (key, val)| {
+       println!("acc: {acc}, val: {val}");
+       if *val == 0. {acc} else { acc - *val * (*val).log2() }})
+}
 
 /// **format_file** -- formats file and deletes all redundant symbol
 /// except:
--- lab2/rust_pr/src/main.rs
+++ lab2/rust_pr/src/main.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use rust_pr::criterions;
+use rust_pr::internals::{calculate_entropy, make_frequency_table, make_n_gram, make_probability_table};
 
 
 const UKR_ALPHABET: [char; 33] = [
@@ -12,11 +13,24 @@ const UKR_ALPHABET: [char; 33] = [
 
 fn main() {
     dotenv().ok();
+    let filepath = std::env::var("OUTPUT_FILENAME").unwrap().as_str().to_string();
 
     // format_file(
     //     std::env::var("SOURCE_FILENAME").unwrap().as_mut_str(),
     //     std::env::var("OUTPUT_FILENAME").unwrap().as_mut_str(),
     // );
+
+    let chunks = 2;
+    let monogram = make_n_gram(chunks, &UKR_ALPHABET);
+    let freq_table1 = make_frequency_table(&filepath, &monogram, chunks);
+    println!("monogram entropy: {}", calculate_entropy(&make_probability_table(&freq_table1)));
+
+    // let bigram = make_n_gram(2,&UKR_ALPHABET);
+    // let frequency_table = make_frequency_table(&filepath, &bigram , 2);
+
+
+    // println!("bigram: {:?} \n\t\t frequency table: {:?}",bigram.len(), frequency_table.len());
+
 
     criterions::criterion_1_0::run();
     criterions::criterion_1_1::run();
